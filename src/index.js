275db11fc27430ba0911e11c502bd39b1bd455df
Jose Nicdao <jonicdao@gmail.com>
2020-04-27 16:19:34 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Metronome from './Metronome';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+    <Metronome />,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
